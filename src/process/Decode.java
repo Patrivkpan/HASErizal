ebac44b550babae60f536d750c761eedd60dafec
ciddarum <cudarum@up.edu.ph>
2017-05-03 21:45:04 +0800

    Fixed decode implementation

--- src/process/Decode.java
+++ src/process/Decode.java
@@ -2,9 +2,10 @@ package process;
 import util.Register;
 
 
-public class Decode extends Thread{
+public class Decode implements Runnable{
 	
 	private static Decode instance;
+	private Thread tInstance;
 	private String instruction[]; // 0 operator, 1 and 2 are the operands
 	private Register dest,src;
 	private String firstUseOfDestRegister, firstUseOfSrcRegister;
@@ -34,6 +35,13 @@ public class Decode extends Thread{
 		}
 
 		//do later=
+	}
+
+	public void start(){
+		if(this.tInstance == null || !this.tInstance.isAlive())
+			this.tInstance = new Thread(this);
+
+		this.tInstance.start();
 	}
 
 	public static Decode getInstance(){
